661247973517024649b5abbd66e628129ffc0ece
DrOtter3000 <info@darknet-online.com>
2025-09-01 16:12:58 +0200

    add basic movement

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ function love.load()
     world = wf.newWorld(0, 400)
 
     player = world:newRectangleCollider(360, 100, 16, 32)
+    player.speed = 300
     
     platform = world:newRectangleCollider(250, 400, 300, 100)
     platform:setType("static")
@@ -11,9 +12,24 @@ end
 
 function love.update(dt)
     world:update(dt)
+
+    local px, py = player:getPosition()
+    if love.keyboard.isDown('right') then
+        player:setX(px + player.speed * dt)
+    end
+    if love.keyboard.isDown('left') then
+        player:setX(px - player.speed * dt)
+    end
 end
 
 
 function love.draw()
     world:draw()
+end
+
+
+function love.keypressed(key)
+    if key == 'up' then
+        player:applyLinearImpulse(0, -250)
+    end
 end
